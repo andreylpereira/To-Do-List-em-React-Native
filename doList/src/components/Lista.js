1b3f883bfcba4d874393851f11736063b277d092
Andrey Luiz Pereira <andreylpereira@gmail.com>
2021-04-10 14:27:35 -0300

    teste de integracao

--- doList/src/components/Lista.js
+++ doList/src/components/Lista.js
@@ -1,42 +1,195 @@
-import React, { useState } from 'react';
-import { View, Text, StyleSheet, TouchableOpacity, FlatList } from 'react-native';
-// Importei flatlist e touchableopacity e axios
+import React, {useState} from 'react';
+import {
+  View,
+  FlatList,
+  Text,
+  StyleSheet,
+  ScrollView,
+  TouchableOpacity,
+} from 'react-native';
+import Icon from 'react-native-vector-icons/Feather';
 import api from '../services/axios';
 
-// Criei método get e fiz adicionei a função onpress
 const Lista = ({navigation}) => {
-  // const LISTAS_MOCK = [
+  // const tarefas = [
   //   {
-  //     nome: "tarefa5",
-  //     descricao: "acordar cedo",
-  //     data: "02 de Fevereiro de 2022"
-  //   }
-  // ]
+  //     nome: 'Comprar pão na casa do joão',
+  //     descricao:
+  //       'compra pão na padaria pão doce pãoaaaaaaaaaaaa aa compra pão na padaria pão doce pãoaaaaaaaaaaaa aacompra pão na padaria pão doce pãoaaaaaaaaaaaa aa compra pão na padaria pão doce pãoaaaaaaaaaaaa aa compra pão na padaria pão doce pãoaaaaaaaaaaaa aa',
+  //     data: '23/10/1950',
+  //   },
+  //   {
+  //     nome:
+  //       'Aula Angular na jamaica com o bob marley doidao de erv',
+  //     descricao:
+  //       'Assistir aula de Angular compra pão na padaria pão doce pãoaaaaaaaaaaaa aa compra pão na padaria pão doce pãoaaaaaaaaaaaa aacompra compra pão na padaria pão doce pãoaaaaaaaaaaaa aa compra pão na padaria pão doce pãoaaaaaaaaaaaa aacompra',
+  //     data: '24/10/1950',
+  //   },
+  //   {
+  //     nome: 'Aula React-native',
+  //     descricao:
+  //       'Assistir aula de React-native compra pão na padaria pão doce pãoaaaaaaaaaaaa aa compra pão na padaria pão doce pãoaaaaaaaaaaaa aacompra',
+  //     data: '25/10/1950',
+  //   },
+  //   {
+  //     nome: 'Aula Banco de dados',
+  //     descricao: 'Assistir aula de Banco de dados',
+  //     data: '26/10/1950',
+  //   },
+  //   {
+  //     nome: 'Aula javascript',
+  //     descricao:
+  //       'Assistir aula de javascript compra pão na padaria pão doce pãoaaaaaaaaaaaa aa compra pão na padaria pão doce pãoaaaaaaaaaaaa aacompra compra pão na padaria pão doce pãoaaaaaaaaaaaa aa compra pão na padaria pão doce pãoaaaaaaaaaaaa aacompra',
+  //     data: '27/10/1950',
+  //   },
+  //   {
+  //     nome: 'Aula javascript',
+  //     descricao:
+  //       'Assistir aula de javascript compra pão na padaria pão doce pãoaaaaaaaaaaaa aa compra pão na padaria pão doce pãoaaaaaaaaaaaa aacompra compra pão na padaria pão doce pãoaaaaaaaaaaaa aa compra pão na padaria pão doce pãoaaaaaaaaaaaa aacompra',
+  //     data: '27/10/1950',
+  //   },
+  //   {
+  //     nome: 'Aula Banco de dados',
+  //     descricao: 'Assistir aula de Banco de dados',
+  //     data: '26/10/1950',
+  //   },
+  // ];
 
   const [listas, setListas] = useState();
 
   const getListas = async () => {
-    try{
+    try {
       const response = await api.get('/');
       console.log(JSON.stringify(response));
       setListas(response.data);
     } catch (error) {
-      console.log("DEU RUIM" + error);
+      console.log('DEU RUIM' + error);
     }
-  }
+  };
 
   const TextListas = ({item}) => {
-    return(
+    return (
       <View>
-        <Text style={css.container}>{item.nome} - {item.descricao} - {item.data}</Text>
+        <View style={css.buttons}>
+          {/* <Icon
+                        name="plus-square"
+                        color={'#410CF5'}
+                        size={24}
+                        style={css.colorAdd}>
+                        <TouchableOpacity
+                          onPress={() =>
+                            navigation.navigate('Cadastrar')
+                          }></TouchableOpacity>
+                      </Icon> */}
+          <Icon name="edit" color={'#410CF5'} size={24} style={css.colorEdi}>
+            <TouchableOpacity
+              onPress={() => navigation.navigate('Editar')}></TouchableOpacity>
+          </Icon>
+          <Icon name="trash-2" color={'#410CF5'} size={24} style={css.colorDel}>
+            <TouchableOpacity
+              onPress={() => navigation.navigate('Editar')}></TouchableOpacity>
+          </Icon>
+        </View>
+        <View style={css.card}>
+          <Text style={css.header}>
+            {obj.item.data} - {obj.item.nome}
+          </Text>
+          <Text style={css.description}>{obj.item.descricao}</Text>
+        </View>
       </View>
-    )
-  } 
-  return(
+    );
+  };
+  return (
     <>
       <View style={css.container}>
-        <Text>To Do List</Text>
+        <View style={css.scroll}>
+          <ScrollView>
+            <FlatList
+              data={listas}
+              renderItem={TextListas}
+              keyExtractor={lista => lista.nome}></FlatList>
+          </ScrollView>
+        </View>
+      </View>
+    </>
+  );
+};
 
+const css = StyleSheet.create({
+  container: {
+    backgroundColor: '#FFFFFF',
+    width: '100%',
+    height: '100%',
+    borderColor: '#410CF5',
+    borderWidth: 2,
+    borderTopWidth: 4,
+  },
+  card: {
+    marginBottom: 25,
+    marginLeft: '5%',
+    width: '90%',
+    backgroundColor: '#F5D100',
+    marginTop: 15,
+    borderWidth: 1,
+    borderRadius: 5,
+    borderColor: '#410CF5',
+    elevation: 15,
+  },
+  header: {
+    textAlign: 'center',
+    margin: 5,
+    borderBottomWidth: 2,
+    borderBottomColor: '#410CF5',
+    fontFamily: 'Montserrat-SemiBoldItalic',
+    fontSize: 17,
+    color: '#410CF5',
+  },
+  description: {
+    textAlign: 'left',
+    margin: 5,
+    marginBottom: 10,
+    fontFamily: 'Montserrat-MediumBold',
+    color: '#410CF5',
+    fontSize: 14,
+  },
+  scroll: {
+    height: '95%',
+    width: '100%',
+    paddingTop: 35,
+  },
+  buttons: {
+    display: 'flex',
+    flexDirection: 'row',
+    marginLeft: '84%',
+    marginBottom: -9.5,
+  },
+  button: {
+    padding: 5,
+    marginLeft: 30,
+    borderColor: '#410CF5',
+    borderRightWidth: 2,
+    borderLeftWidth: 2,
+    borderTopWidth: 2,
+    borderBottomWidth: 2,
+  },
+  colorAdd: {
+    color: '#19B354',
+  },
+  colorDel: {
+    color: '#FA2201',
+  },
+  colorEdi: {
+    color: '#4675C2',
+    // marginTop: -1,
+    marginLeft: 1,
+  },
+});
+
+export default Lista;
+
+{
+  /* <View style={css.container}>
+        <Text>To Do List</Text>
         <View>
         <TouchableOpacity style={css.button} onPress={() => navigation.navigate('Cadastro')}>
             <Text style={css.buttonText}>Cadastrar</Text>
@@ -50,66 +203,7 @@ const Lista = ({navigation}) => {
         data={listas}
         renderItem={TextListas}
         keyExtractor={ lista => lista.nome }
-        
-        
         ></FlatList>
       </View>
-    </>
-  );
+    </> */
 }
-
-const css = StyleSheet.create({
-
-  container: {
-    backgroundColor: '#EDEDE9',
-    flex: 1,
-    height: '100%',
-    borderColor: '#410CF5',
-    borderWidth: 2,
-    borderTopWidth: 4,
-    alignContent: 'center',
-    justifyContent: 'center',
-    alignItems: 'center',
-    fontFamily: 'Montserrat-Regular',
-  },
-  header: {
-    fontSize: 42,
-    marginBottom: 15
-  },
-  input:{
-    width: 200,
-    height: 40,
-    marginVertical: 10,
-    borderColor:'black',
-    borderWidth: 1,
-    borderRadius: 20,
-    paddingLeft: 20
-  },
-  button: {
-    borderColor: '#ffd700',
-    borderWidth: 1,
-    borderRadius: 1,
-    backgroundColor: '#ffd700',
-    width: 100,
-    height: 25,
-    alignItems: 'center',
-    marginTop: 15,
-  },
-  buttonText:{
-    color:"#000"
-  },
-  cervejaNome: {
-    borderColor: '#9e9e9e',
-    borderTopWidth: 1,
-    paddingVertical: 20,
-    width: 200,
-    textAlign: 'center'
-  },
-  tituloLista:{
-    fontSize: 24,
-    marginTop: 30,
-    marginBottom: 20
-  }
-});
-
-export default Lista;
